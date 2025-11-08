bc57fb0ad11a75eb0bde1ad553dea9d19dd70e06
Sara <saramagalhaes242@gmail.com>
2025-11-08 10:49:03 -0300

    Usando realmente compreensao de listas

--- Basico/Compreensao_de_listas/inicio.py
+++ Basico/Compreensao_de_listas/inicio.py
@@ -6,3 +6,9 @@ for numero in numeros:
     numeros_dobrados.append(numero * 2)
 
 print(numeros_dobrados)
+
+#com compreensao de listas
+numeros_2 = [1, 2, 3, 4, 5]
+numeros_dobrados_2 = [numero_2 *  2 for numero_2 in numeros_2]
+
+print(numeros_dobrados_2)
