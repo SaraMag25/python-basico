606f78d369bb2c28d1968ea5a1269e7a47cdcb72
Sara <saramagalhaes242@gmail.com>
2025-11-08 10:57:20 -0300

    melhorando as coisas

--- Basico/Compreensao_de_listas/inicio.py
+++ Basico/Compreensao_de_listas/inicio.py
@@ -12,3 +12,13 @@ numeros_2 = [1, 2, 3, 4, 5]
 numeros_dobrados_2 = [numero_2 *  2 for numero_2 in numeros_2]
 
 print(numeros_dobrados_2)
+
+#melhorando
+numeros_3 = [1, 2, 3, 4, 5]
+
+def dobro(numero_3):
+    return numero_3 * 2
+
+numeros_dobrados_3 = [dobro(numero_3) for numero_3 in numeros_3]
+
+print(numeros_dobrados_3)
