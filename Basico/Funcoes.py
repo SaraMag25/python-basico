caa637279f3025ce0bf83609de58a4e99bab7ce3
Sara <saramagalhaes242@gmail.com>
2025-11-09 10:21:39 -0300

    Funcoes

---
+++ Basico/Funcoes.py
@@ -0,0 +1,10 @@
+def somar(a, b):
+    resultado = a + b
+    return resultado
+print(somar(1, 2))
+
+def envia_email():
+    email = 'naoexiste@gmail.com',
+    senha = '1234'
+    email_envio = 'outroteste@gmail.com'
+
