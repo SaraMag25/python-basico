b1d625127cb64325396a4832bae91753c4df57a6
Sara <saramagalhaes242@gmail.com>
2025-11-09 09:45:45 -0300

    utilizando dicionarios de modo inteligente

--- Basico/Dicionario.py
+++ Basico/Dicionario.py
@@ -6,4 +6,28 @@ print(meu_dicio.pop('idade'))
 print(meu_dicio.keys())
 print(meu_dicio.values())
 print(meu_dicio.clear())
-print(meu_dicio) 
+print(meu_dicio)
+
+#melhorando as coisas
+pessoa = {
+    'nome' : 'Sara',
+    'idade' : 20,
+    'status' : 'viva',
+    'interesses' : ['python', 'javascript', 'java'],
+    'mascote' : {
+        'gatas' : {
+            'nome' : ['Maria', 'polly', 'Leia'],
+        },
+        'cachorros' : {
+            'nome' : ['Tobi', 'Rosa'],
+        }
+    }
+}
+print(pessoa.get('interesses')[0])
+print(pessoa.get('mascote').get('gatas').get('nome'))
+pessoa['ano nascimento'] = 2006
+pessoa['mae'] = {
+    'nome' : 'Elizete',
+    'idade' : 42
+}
+print(pessoa)
