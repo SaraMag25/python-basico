3fd3a49a6737fc22dd44b85322d192070ff52473
Sara <saramagalhaes242@gmail.com>
2025-11-09 10:40:21 -0300

    mostrando o nome e o email da pessoa

--- Basico/Funcoes.py
+++ Basico/Funcoes.py
@@ -8,3 +8,28 @@ def envia_email():
     senha = '1234'
     email_envio = 'outroteste@gmail.com'
 
+#exemplo de como usar uma funcao que nao retorna nada
+pessoas = ['Sara', 'Fabricio', 'Clara', 'Maria']
+for p in pessoas:
+    envia_email()
+
+pessoas_2 = [
+    {'nome' : 'Sara',
+     'email': 'naoexiste@gmail.com'
+    },
+    {'nome' : 'Clara',
+     'email': 'naoexisteClara@gmail.com'
+    }
+]
+
+#mostra o nome e o email da pessoa
+
+def envia_email_2(nome, email):
+    nome_dest = nome
+    email_envio = email
+    return f'Email enviado para {nome}, dono do email {email}'
+
+
+for p in pessoas_2:
+    email_enviado = envia_email_2(p['nome'], p['email'])
+    print(email_enviado)
