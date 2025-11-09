ffad1ac6e5d788e0e413f1533da5f2250e72d3bc
Sara <saramagalhaes242@gmail.com>
2025-11-09 09:27:46 -0300

    dicionarios em python

---
+++ Basico/Dicionario.py
@@ -0,0 +1,9 @@
+#testando alguns metodos
+meu_dicio = {'nome' : 'Sara', 'idade' : 19, 'faculdade' : 'eng comp'}
+print(meu_dicio) 
+print(meu_dicio.get('nome'))
+print(meu_dicio.pop('idade'))
+print(meu_dicio.keys())
+print(meu_dicio.values())
+print(meu_dicio.clear())
+print(meu_dicio) 
